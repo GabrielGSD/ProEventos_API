ba3abe2379c554c43ac666fe3ca74027a84f8421
myusername <gabrieldaniel@gec.inatel.br>
2021-09-01 14:33:20 -0300

    Add AutoMapper and Data Annotations

--- src/ProEventos.API/Controllers/EventoController.cs
+++ src/ProEventos.API/Controllers/EventoController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProEventos.Application.Contratos;
-using ProEventos.Domain;
-using ProEventos.Persistence.Contextos;
+using ProEventos.Application.Dtos;
 
 namespace ProEventos.API.Controllers
 {
@@ -29,7 +27,8 @@ namespace ProEventos.API.Controllers
       try
       {
           var eventos = await _eventoService.GetAllEventosAsync(true);
-          if(eventos == null) return NotFound("Nenhum evento encontrado.");
+          if(eventos == null) return NoContent();
+
           return Ok(eventos);
       }
       catch (Exception error)
@@ -44,7 +43,7 @@ namespace ProEventos.API.Controllers
       try
       {
           var evento = await _eventoService.GetEventoByIdAsync(id, true);
-          if(evento == null) return NotFound("Nenhum evento encontrado.");
+          if(evento == null) return NoContent();
           return Ok(evento);
       }
       catch (Exception error)
@@ -59,7 +58,7 @@ namespace ProEventos.API.Controllers
       try
       {
           var evento = await _eventoService.GetAllEventosByTemaAsync(tema, true);
-          if(evento == null) return NotFound("Nenhum evento por tema encontrado.");
+          if(evento == null) return NoContent();
           return Ok(evento);
       }
       catch (Exception error)
@@ -69,7 +68,7 @@ namespace ProEventos.API.Controllers
     }
 
     [HttpPost]
-    public async Task<IActionResult> Post(Evento model)
+    public async Task<IActionResult> Post(EventoDto model)
     {
       try
       {
@@ -84,7 +83,7 @@ namespace ProEventos.API.Controllers
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Evento model)
+    public async Task<IActionResult> Put(int id, EventoDto model)
     {
       try
       {
@@ -103,9 +102,12 @@ namespace ProEventos.API.Controllers
     {
       try
       {
+          var evento = await _eventoService.GetEventoByIdAsync(id, true);
+          if(evento == null) return NoContent();
+
           return await _eventoService.DeleteEvento(id) 
             ? Ok("Deletado") 
-            : BadRequest("Evento não deletado.");
+            : throw new Exception("Ocorreu um problema não específico ao tentar deletar Evento.");
       }
       catch (Exception error)
       {
---
+++ src/ProEventos.Application/Dtos/EventoDto.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProEventos.Application.Dtos
+{
+    public class EventoDto
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório"),
+         StringLength(50, MinimumLength = 50, ErrorMessage = "O campo {0} deve ter de 3 a 50 caracteres.")]
+        public string Tema { get; set; }
+
+        [Display(Name = "Qtd Pessoas."),
+         Range(1, 12000, ErrorMessage = "{0} não pode ser menor que 1 nem maior que 120.000.")]
+        public int QdtPessoas { get; set; }
+
+        [RegularExpression(@".*\.(gif|jpe?g|bmp|png)$", ErrorMessage = "Não é uma imagem válida. (gif, jpeg, jpg, bmp, png)")]
+        public string ImagemURL { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório."),
+         Phone(ErrorMessage = "O campo {0}  está com o número inválido.")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório"),
+         Display(Name = "e-mail"),
+         EmailAddress(ErrorMessage = "O campo {0} precisa ser válido")]
+        public string Email { get; set; }
+        public IEnumerable<LoteDto> Lotes { get; set; }
+        public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
+        public IEnumerable<PalestranteDto> PalestrantesEventos { get; set; }
+    }
+}
