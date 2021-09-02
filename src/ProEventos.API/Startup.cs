50d2b8841b030c1c1e64bd34d4ef4a1381e0a9b4
myusername <gabrieldaniel@gec.inatel.br>
2021-09-02 11:34:34 -0300

    Add config to upload images

--- src/ProEventos.API/Startup.cs
+++ src/ProEventos.API/Startup.cs
@@ -12,6 +12,9 @@ using ProEventos.Persistence.Contextos;
 using ProEventos.Persistence.Contratos;
 using AutoMapper;
 using System;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace ProEventos.API
 {
@@ -66,6 +69,12 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Configuração para upload de imagens
+            app.UseStaticFiles(new StaticFileOptions() {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Resources")),
+                RequestPath = new PathString("/Resources")
+            });
 
             app.UseEndpoints(endpoints =>
             {
